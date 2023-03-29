aee5b845a0a5b7ee37e16763eca59ccf5bf80e33
Andreas Gysin <andreas@ertdfgcvb.ch>
2023-03-29 11:59:01 +0200

    Update sketch.js

--- esempi/mp_hands/3_landmarks_p5/src/sketch.js
+++ esempi/mp_hands/3_landmarks_p5/src/sketch.js
@@ -2,31 +2,31 @@ let capture
 let detector
 
 async function setup() {
-  
+
 	createCanvas(640, 480)
 
 	capture = createCapture(VIDEO)
 	capture.size(640, 480)
 	capture.hide()
-	
+
 	console.log("Carico modello...")
 	detector = await createDetector()
 	console.log("Modello caricato.")
 }
 
 async function draw() {
-	
+
 	background(255)
 
 	// noStroke()
 	// fill(255, 10)
-	// rect(0, 0, width, height) 
+	// rect(0, 0, width, height)
 
 	// Disegna la webcam sullo stage, a specchio
-	// push()
-	// scale(-1, 1)
-	// image(capture, -640, 0)
-	// pop()
+	push()
+	scale(-1, 1)
+	image(capture, -640, 0)
+	pop()
 
 	if (detector && capture.loadedmetadata) {
 		const hands = await detector.estimateHands(capture.elt, { flipHorizontal: true })
@@ -36,19 +36,19 @@ async function draw() {
 			const handedness = hand.handedness // Left : Right
 			// for (let i=0; i<hand.keypoints.length; i++) {
 			// 	const k = hand.keypoints[i]
-			// 	// const name = k.name.split('_')[0].toString().toLowerCase()				
+			// 	// const name = k.name.split('_')[0].toString().toLowerCase()
 			// 	noStroke()
 			// 	fill(random(255), random(255), random(255))
 			// 	ellipse(k.x, k.y, 12)
 			// }
 
 			noStroke()
-			fill(255,0,0) 
+			fill(255,0,0)
 			for (let i=0; i<5; i++) {
 				const k = hand.keypoints[i * 4 + 4]
-				ellipse(k.x, k.y, 20, 20)
+				ellipse(k.x, k.y, 50, 50)
 			}
-		}		
+		}
 	}
 }
 
