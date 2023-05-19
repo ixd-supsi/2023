da295e4d6ad2e8d146fca49169cbef4edf38f414
Andreas Gysin <andreas@ertdfgcvb.ch>
2023-05-19 10:58:33 +0200

    Crea esempio sound synth

---
+++ esempi/esempi_p5js/19_sound_synth/sketch.js
@@ -0,0 +1,12 @@
+function setup(){    
+    createCanvas(windowWidth, windowHeight)
+    background(255)
+}
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight)
+}
+
+function draw(){   
+   
+}
